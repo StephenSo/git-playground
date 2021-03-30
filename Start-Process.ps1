2634b9897671a03b1c3a99587800d5b6f9c32a8f
Stephen.S <ss@ss.com>
2021-03-30 11:45:28 +0100

    Update to Read-File data

--- Start-Process.ps1
+++ Start-Process.ps1
@@ -1,3 +1,9 @@
 write-host "Main Branch"
 $UserInput = Read-Host "Enter data input"
 
+$FileData = Get-Content $UserInput
+$i = 0
+foreach ($Line in $FileData){
+    $i++
+    Write-Host "$($i)-$($Line)"
+}
